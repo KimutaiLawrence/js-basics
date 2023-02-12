c82859a6a31be4a81dd3be0a5454f3192500de48
Kimutai Lawrence <kimutai.lawrence19@students.dkut.ac.ke>
2023-02-13 00:58:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const x = 9;
+
+const color = x > 10 ? 'red' : 'blue';
+
+switch(color) {
+        case 'red':
+         console.log('color is red');
+         break;
+        case 'blue':
+         console.log('color is blue');
+         break;
+       default:
+         console.log('color is NOT red or blue ')
+}  
